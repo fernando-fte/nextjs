aa100723944b4469bd6573afed03c99b5f2092b9
fernando-fte <fernando.fte@gmail.com>
2022-07-06 14:49:43 -0300

    Set header

--- pages/api/proposal.js
+++ pages/api/proposal.js
@@ -4,6 +4,8 @@ async function proposal (rea, res) {
     const proposalResponseJson = await proposalResponse.json();
     const proposal_prince = proposalResponseJson.data.cost_value
 
+    res.setHeader('Cahe-Control', 's-maxage=10, stale-white-revalidate')
+
     res.json({
         proposal: proposal_prince
     });
